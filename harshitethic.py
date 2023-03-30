0dc821a4eec150503428d4095b04296498cde4b4
Dhiraj Raj <81546814+drjdhiraj@users.noreply.github.com>
2023-03-30 15:44:17 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,14 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "Done! Congratulations on your new bot. You will find it at t.me/drjdhirajbot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+
+Use this token to access the HTTP API:
+6063629900:AAEef7YTd0E-jp_shTvSNqk_nH1l3yNCiYY
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+For a description of the Bot API, see this page: https://core.telegram.org/bots/api
+"
 else:
     tgkey = tgenv
 print(tgenv)
